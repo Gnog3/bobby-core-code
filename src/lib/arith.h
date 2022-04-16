2a7379bc8b2df8ffac2250d615c57f25c7a15ba5
TheWildJarvi <44104303+TheWildJarvi@users.noreply.github.com>
2022-04-16 14:20:49 -0400

    Update arith.h
    added hardware division support
    @Gnog3 please add to the c std lib support

--- src/lib/arith.h
+++ src/lib/arith.h
@@ -21,4 +21,31 @@ inline int mul_hi(void)
     return csr_read(1043);
 }
 
+inline void send_div(int valA, int valB)
+{
+	csr_write(1044, valA);
+	csr_write(1045, valB);
+	__asm__("nop");
+	__asm__("nop");
+	__asm__("nop");
+	__asm__("nop");
+	__asm__("nop");
+	__asm__("nop");
+	__asm__("nop");
+	__asm__("nop");
+	__asm__("nop");
+	__asm__("nop");
+	__asm__("nop");
+}
+
+inline int div_q(void)
+{
+    return csr_read(1046);
+}
+
+inline int div_r(void)
+{
+    return csr_read(1047);
+}
+
 #endif
